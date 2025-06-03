157029e77dccaa3af72dd3aec10a966fe3bcec47
sabahassaad19 <121083063+Oussama-Noeman@users.noreply.github.com>
2025-06-03 11:12:17 +0300

    add timestamps to order model

--- models/Order.js
+++ models/Order.js
@@ -1,60 +1,65 @@
 const mongoose = require("mongoose");
 
-const orderSchema = new mongoose.Schema({
-  code: {
-    type: String,
-    required: true,
-  },
-  items_count: {
-    type: Number,
-    required: true,
-  },
-  user_id: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-  },
-  customer_name: {
-    type: String,
-  },
-  phone: {
-    type: String,
-    required: true,
-  },
-  district: {
-    type: String,
-  },
-  address: {
-    type: String,
-    required: true,
-  },
-  products: [
-    {
-      id: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Product",
-        required: true,
-      },
-      quantity: {
-        type: Number,
-        required: true,
-        default: 1,
-      },
+const orderSchema = new mongoose.Schema(
+  {
+    code: {
+      type: String,
+      required: true,
     },
-  ],
-  extra_fees: {
-    type: String,
-    required: true,
+    items_count: {
+      type: Number,
+      required: true,
+    },
+    user_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    customer_name: {
+      type: String,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    district: {
+      type: String,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    products: [
+      {
+        id: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+          default: 1,
+        },
+      },
+    ],
+    extra_fees: {
+      type: String,
+      required: true,
+    },
+    total_price: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "accepted", "rejected"],
+      default: "pending",
+    },
   },
-  total_price: {
-    type: String,
-    required: true,
-  },
-  status: {
-    type: String,
-    enum: ["pending", "accepted", "rejected"],
-    default: "pending",
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Order = mongoose.model("Order", orderSchema);
 
