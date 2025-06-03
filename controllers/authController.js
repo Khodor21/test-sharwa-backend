62f35ef52cca8dcde01dc8124a298fbc66eacdfd
khodorhasan21 <khodorhasan17@gmail.com>
2025-06-03 17:52:37 +0300

    My chnages

--- controllers/authController.js
+++ controllers/authController.js
@@ -122,6 +122,8 @@ const deleteAccount = async (req, res) => {
     res.clearCookie("token", {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      path: "/",
     });
     res.status(200).json({ message: "Account deleted successfully" });
   } catch (error) {
