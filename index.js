c57f7666bedcc1d60437b9525d14bce89426d662
khodorhasan21 <khodorhasan17@gmail.com>
2025-06-19 18:50:47 +0300

    mmmmmmmm

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const mainSectionRoutes = require("./routes/mainSectionRoutes");
 const orderRoutes = require("./routes/orderRoutes");
 const configurationRoutes = require("./routes/configurationRoutes");
 const heroRoutes = require("./routes/heroRoutes");
-const adminRoutes = require("./routes/adminRoutes");
 
 const cookieParser = require("cookie-parser");
 const connectDB = require("./config/db");
@@ -35,7 +34,6 @@ app.use("/api", mainSectionRoutes);
 app.use("/api", orderRoutes);
 app.use("/api", configurationRoutes);
 app.use("/api", heroRoutes);
-app.use("/api", adminRoutes);
 
 connectDB();
 
